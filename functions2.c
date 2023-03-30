4808018f2178c4cd740beab57d85cab74cb7a6fc
YoussefZiali <122979076+YoussefZiali@users.noreply.github.com>
2023-03-30 14:28:01 +0000

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -2,6 +2,7 @@
 
 /****************** PRINT POINTER ******************/
 /**
+ * edited
  * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
